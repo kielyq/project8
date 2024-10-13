fa4141367a2588f1b7ee31ab81ba748905d568da
xiesh <156837234+kielyq@users.noreply.github.com>
2024-10-13 17:44:48 +0500

    added styles

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -6,13 +6,13 @@
     @foreach ($reports as $arr)
         <div class="shadow-md w-64 m-16 " >
             <div class="card-text m-6 ">
-                <p class="id_of_rep">
+                <p class="font-sans text-lg font-normal text-sky-600">
                 {{$arr['id']}}
                 </p>
-                <p class="title-card">
+                <p class="font-sans font-medium leading-relaxed text-pretty text-sky-900">
                 {{$arr['description']}}
                 </p>
-                <p class="price-card">
+                <p class="font-sans text-sm font-normal inline-block align-text-bottom pt-2 text-cyan-900">
                 {{$arr['create_at']}}
                 </p>
             </div>
