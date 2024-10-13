f4e59a8174a7e96b77480c1873895e4a124e261c
xiesh <156837234+kielyq@users.noreply.github.com>
2024-10-13 16:52:28 +0500

    ch

--- app/Models/Statuses.php
+++ app/Models/Statuses.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class statuses extends Model
+class Statuses extends Model
 {
     use HasFactory;
 }
