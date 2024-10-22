fd091fd8eee8dd6d90bc38b294971554a89285c8
xiesh <156837234+kielyq@users.noreply.github.com>
2024-10-22 17:21:49 +0500

    изменение индекса и репорта

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Reports;
+use App\Models\Report;
 
 
 class ReportController extends Controller
 {
   public function index(){
-    $reports = Reports::all(); // выборка всех данных из таблицы reports.
+    $reports = Report::all(); // выборка всех данных из таблицы reports.
     return view('report.index', compact('reports'));
   }
 }
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -7,13 +7,13 @@
         <div class="shadow-md w-64 m-16 " >
             <div class="card-text m-6 ">
                 <p class="font-sans text-lg font-normal text-sky-600">
-                {{$arr['id']}}
+                {{$arr->id}}
                 </p>
                 <p class="font-sans font-medium leading-relaxed text-pretty text-sky-900">
-                {{$arr['description']}}
+                {{$arr->description}}
                 </p>
                 <p class="font-sans text-sm font-normal inline-block align-text-bottom pt-2 text-cyan-900">
-                {{$arr['create_at']}}
+                {{$arr->create_at}}
                 </p>
             </div>
         </div>
